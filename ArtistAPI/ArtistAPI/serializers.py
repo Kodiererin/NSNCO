4a782d74cbba1a7373553805c6e65de5b88e21e9
Kodiererin <iamujjwal45@gmail.com>
2023-07-24 10:38:00 +0530

    Adding Serializers and Views

--- ArtistAPI/ArtistAPI/serializers.py
+++ ArtistAPI/ArtistAPI/serializers.py
@@ -1,14 +1,44 @@
-from rest_framework import serializers
+from rest_framework import generics, filters, status
+from rest_framework.response import Response
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny
+from django.contrib.auth.models import User
 from .models import Artist, Work
+from .serializers import ArtistSerializer, WorkSerializer, UserRegistrationSerializer
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework_simplejwt.views import TokenObtainPairView
+from django.contrib.auth.models import User
+from .models import Artist, Work
+from .serializers import ArtistSerializer, WorkSerializer, UserRegistrationSerializer
 
-class WorkSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Work
-        fields = '__all__'
+class WorkListCreateView(generics.ListCreateAPIView):
+    queryset = Work.objects.all()
+    serializer_class = WorkSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['work_type']
 
-class ArtistSerializer(serializers.ModelSerializer):
-    works = WorkSerializer(many=True, read_only=True)
+class ArtistListCreateView(generics.ListCreateAPIView):
+    queryset = Artist.objects.all()
+    serializer_class = ArtistSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['name']
 
-    class Meta:
-        model = Artist
-        fields = ['id', 'name', 'works']
+@api_view(['POST'])
+@permission_classes([AllowAny])
+def register_user(request):
+    if request.method == 'POST':
+        serializer = UserRegistrationSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            token, _ = Token.objects.get_or_create(user=user)
+            return Response({'token': token.key, 'user_id': user.pk}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+# Custom exception handler
+def custom_exception_handler(exc, context):
+    response = exception_handler(exc, context)
+
+    if response is not None:
+        response.data['status_code'] = response.status_code
+
+    return response
